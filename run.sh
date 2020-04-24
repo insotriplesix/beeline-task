7f0b042f8dd55feb9fe6992b01c392a3ed295973
5aboteur <5aboteur@protonmail.com>
2020-04-25 01:47:21 +0700

    Add openjdk check

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo -ne "Check for openjdk package... "
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' openjdk-8-jre | grep "install ok installed")
+
+if [ "" == "$PKG_OK" ]; then
+	echo "the package wasn't found in the system. Abort."
+	exit 1
+fi
+
+echo "OK."
+
 echo -ne "Check for maven package... "
 
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' maven | grep "install ok installed")
