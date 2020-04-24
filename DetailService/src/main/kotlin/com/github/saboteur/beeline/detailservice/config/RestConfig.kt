a2103c68f974a62bb7ca2bfe7ddb6c8525a0ce28
5aboteur <5aboteur@protonmail.com>
2020-04-24 18:00:17 +0700

    Mvc to Flux

--- DetailService/src/main/kotlin/com/github/saboteur/beeline/detailservice/config/RestConfig.kt
+++ DetailService/src/main/kotlin/com/github/saboteur/beeline/detailservice/config/RestConfig.kt
@@ -1,6 +1,5 @@
 package com.github.saboteur.beeline.detailservice.config
 
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
@@ -9,8 +8,6 @@ import org.springframework.web.client.RestTemplate
 class RestConfig {
 
     @Bean
-    fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate {
-        return restTemplateBuilder.build()
-    }
+    fun restTemplate(): RestTemplate = RestTemplate()
 
 }
