4da7e6f1c2942fa9a9e461cfa2e2b6044cc4b159
5aboteur <5aboteur@protonmail.com>
2020-04-25 00:20:51 +0700

    Add packages check and project build process

--- run.sh
+++ run.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
-sudo apt install docker-compose
+echo -ne "Check for maven package... "
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' maven | grep "install ok installed")
+
+if [ "" == "$PKG_OK" ]; then
+	echo "the package wasn't found in the system. Abort."
+	exit 1
+fi
+
+echo "OK."
+
+echo -ne "Check for docker package... "
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' docker | grep "install ok installed")
+
+if [ "" == "$PKG_OK" ]; then
+	echo "the package wasn't found in the system. Abort."
+	exit 1
+fi
+
+echo "OK."
+
+echo -ne "Check for docker-compose package... "
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' docker-compose | grep "install ok installed")
+
+if [ "" == "$PKG_OK" ]; then
+	echo "the package wasn't found in the system. Abort."
+	exit 1
+fi
+
+echo "OK."
+echo "Build the project."
+
+cd DetailService/
+mvn clean package
+
+cd ../
+
+cd ProfileService/
+mvn clean package
+
+cd ../
 
 docker-compose up --build -d
