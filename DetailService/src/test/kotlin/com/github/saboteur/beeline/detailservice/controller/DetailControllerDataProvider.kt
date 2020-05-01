0c68cd6d05b52886c404ff61fb32838330d315d6
5aboteur <5aboteur@protonmail.com>
2020-05-02 03:25:20 +0700

    Add mockk test for controller

---
+++ DetailService/src/test/kotlin/com/github/saboteur/beeline/detailservice/controller/DetailControllerDataProvider.kt
@@ -0,0 +1,25 @@
+package com.github.saboteur.beeline.detailservice.controller
+
+import com.github.saboteur.beeline.detailservice.dto.CallerProfile
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import java.util.stream.Stream
+
+class DetailControllerDataProvider {
+
+    class GetCallerProfile : ArgumentsProvider {
+        override fun provideArguments(extensionContext: ExtensionContext?): Stream<out Arguments> {
+            val providedCallerProfile = CallerProfile(
+                ctn = "1234567890",
+                callerId = "03e17537-30de-4598-a816-108945fa68b4",
+                name = "Pavel Durov",
+                email = "durov@hotmail.com"
+            )
+
+            return Stream.of(Arguments.of(providedCallerProfile))
+        }
+
+    }
+
+}
---
+++ DetailService/src/test/kotlin/com/github/saboteur/beeline/detailservice/controller/DetailControllerTest.kt
@@ -0,0 +1,81 @@
+package com.github.saboteur.beeline.detailservice.controller
+
+import com.github.saboteur.beeline.detailservice.dto.CallerProfile
+import com.github.saboteur.beeline.detailservice.service.DetailService
+import com.github.saboteur.beeline.detailservice.service.impl.DetailServiceImpl
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ArgumentsSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpHeaders
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@ExtendWith(MockKExtension::class)
+@WebFluxTest(DetailServiceControllerApi::class)
+class DetailControllerTest {
+
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        fun detailService() = mockk<DetailServiceImpl>()
+    }
+
+    @Autowired
+    lateinit var webClient: WebTestClient
+
+    @Autowired
+    lateinit var detailService: DetailService
+
+    @Test
+    fun getCallerProfileByCellIdWhenNoDataFound() {
+        every {
+            detailService.getCallerProfile(any())
+        } returns emptyList()
+
+        webClient
+            .get()
+            .uri("/api/v1/detailservice/getCallerProfileByCellId?cellId={cellId}", "TEST")
+            .header(HttpHeaders.ACCEPT, "application/json")
+            .exchange()
+            .expectStatus().isNotFound
+            .expectBody()
+            .json("[]")
+
+        verify(atLeast = 1) {
+            detailService.getCallerProfile(any())
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DetailControllerDataProvider.GetCallerProfile::class)
+    fun getCallerProfileByCellIdWhenDataProvided(providedCallerProfile: CallerProfile) {
+        every {
+            detailService.getCallerProfile(any())
+        } returns listOf(providedCallerProfile)
+
+        webClient
+            .get()
+            .uri("/api/v1/detailservice/getCallerProfileByCellId?cellId={cellId}", "TEST")
+            .header(HttpHeaders.ACCEPT, "application/json")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$[0].ctn").isEqualTo(providedCallerProfile.ctn)
+            .jsonPath("$[0].callerId").isEqualTo(providedCallerProfile.callerId)
+            .jsonPath("$[0].name").isEqualTo(providedCallerProfile.name)
+            .jsonPath("$[0].email").isEqualTo(providedCallerProfile.email)
+
+        verify(atLeast = 1) {
+            detailService.getCallerProfile(any())
+        }
+    }
+
+}
