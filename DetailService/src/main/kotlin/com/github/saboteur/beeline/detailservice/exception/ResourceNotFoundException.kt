8135aa72814d3f47ad0e867e4b7e0b4f233ae057
5aboteur <5aboteur@protonmail.com>
2020-05-01 15:46:13 +0700

    Add service interface layer + exception fixes

--- DetailService/src/main/kotlin/com/github/saboteur/beeline/detailservice/exception/ResourceNotFoundException.kt
+++
@@ -1,9 +0,0 @@
-package com.github.saboteur.beeline.detailservice.exception
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-class ResourceNotFoundException(
-    override val message: String
-): Exception(message)
--- DetailService/src/main/kotlin/com/github/saboteur/beeline/detailservice/service/DetailService.kt
+++ DetailService/src/main/kotlin/com/github/saboteur/beeline/detailservice/service/DetailService.kt
@@ -1,70 +1,7 @@
 package com.github.saboteur.beeline.detailservice.service
 
-import com.github.saboteur.beeline.detailservice.config.properties.RestProperties
 import com.github.saboteur.beeline.detailservice.dto.CallerProfile
-import com.github.saboteur.beeline.detailservice.dto.external.ProfileDto
-import com.github.saboteur.beeline.detailservice.mapper.ProfileToCallerProfileMapper
-import com.github.saboteur.beeline.detailservice.repository.SessionRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import mu.KotlinLogging
-import org.springframework.stereotype.Service
-import org.springframework.web.client.RestClientResponseException
-import org.springframework.web.client.RestTemplate
-import java.util.concurrent.*
-import kotlin.system.measureTimeMillis
 
-@Service
-class DetailService(
-    private val sessionRepository: SessionRepository,
-    private val restProperties: RestProperties,
-    private val restTemplate: RestTemplate
-) {
-
-    fun getCallerProfile(cellId: String): List<CallerProfile> {
-        val ctns = sessionRepository.findAllCtnByCellId(cellId)
-
-        if (ctns.isEmpty()) {
-            logger.info { "CTNs for Cell ID = $cellId weren't found in database" }
-            return emptyList()
-        }
-
-        val result = ConcurrentLinkedQueue<CallerProfile>()
-
-        measureTimeMillis {
-            runBlocking {
-                ctns.forEach { ctn ->
-                    launch(Dispatchers.IO) {
-                        val url =
-                            StringBuilder()
-                                .append(restProperties.profileServiceUrl)
-                                .append("/getProfileByCtn?ctn=$ctn")
-                                .toString()
-
-                        logger.debug { "Thread = ${Thread.currentThread().name}, URL = $url" }
-
-                        try {
-                            val response = restTemplate.getForObject(url, ProfileDto::class.java)
-                            if (response != null)
-                                result.add(ProfileToCallerProfileMapper[response])
-                        } catch (e: RestClientResponseException) {
-                            logger.error { e.localizedMessage }
-                        }
-                    }
-                }
-            }
-        }.also { time ->
-            logger.info {
-                "Request for Cell ID = $cellId was processed in $time ms"
-            }
-        }
-
-        return result.toList()
-    }
-
-    companion object {
-        private val logger = KotlinLogging.logger {}
-    }
-
+interface DetailService {
+    fun getCallerProfile(cellId: String): List<CallerProfile>
 }
